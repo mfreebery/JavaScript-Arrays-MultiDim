29156681331f5212646d32bbb8323b5d7e646edb
Michael Leonhard <77648651+MSLeonhard@users.noreply.github.com>
2021-02-12 12:06:31 -0500

    added lives text in prompts

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -6,16 +6,20 @@ let minesweeper = [
     ['O', 'O', 'O'],
 ];
 
-let userRow = prompt ('What row do you think the mine is in?? Guess between 0 and 2!');
-let userCol = prompt ('What column do you think the mine is in?? Guess between 0 and 2!');
+let userGuesses = [
 
-for (i = 0; i < 4; i++) {
+]
+
+let userRow = prompt ('What row do you think the mine is in?? Guess 0, 1, or 2! You have 3 guesses!');
+let userCol = prompt ('What column do you think the mine is in?? Guess 0, 1, or 2! You have 3 guesses!');
+
+for (i = 2; i > 0; i--) {
     if ( minesweeper[userRow][userCol] === minesweeper [1][0] ) {
         alert ('You found the mine! Great job!')
+        break
     }
     else {
-        userRow = prompt ('What row do you think the mine is in?? Guess between 0 and 2!');
-        userCol = prompt ('What column do you think the mine is in?? Guess between 0 and 2!');
+        userRow = prompt (`What row do you think the mine is in?? Guess 0, 1, or 2! You have ${i} guesses left.`);
+        userCol = prompt (`What column do you think the mine is in?? Guess 0, 1 , or 2! You have ${i} guesses left.`);
     }
-   
 }
