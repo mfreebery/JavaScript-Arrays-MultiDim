a3e6c95099861f511d4445712f37d5eaa27300a3
Matt Freebery <74575157+mfreebery@users.noreply.github.com>
2021-02-12 11:52:34 -0500

    completed step 2 adding game contents to MS

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -5,3 +5,17 @@ let minesweeper = [
     ['X', 'O', 'O',],
     ['O', 'O', 'O'],
 ];
+
+let userRow = prompt ('What row do you think the mine is in?? Guess between 0 and 2!');
+let userCol = prompt ('What column do you think the mine is in?? Guess between 0 and 2!');
+
+for (i = 0; i < 4; i++) {
+    if ( minesweeper[userRow][userCol] === minesweeper [1][0] ) {
+        alert ('You found the mine! Great job!')
+    }
+    else {
+        userRow = prompt ('What row do you think the mine is in?? Guess between 0 and 2!');
+        userCol = prompt ('What column do you think the mine is in?? Guess between 0 and 2!');
+    }
+   
+}
